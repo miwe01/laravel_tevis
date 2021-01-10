36d1487ab6f7041387162fe0d3df6388c8efa782
Tom <tom.becker@alumni.fh-aachen.de>
2021-01-10 13:17:50 +0100

    Änderung Templates /Konto und update tutor

---
+++ resources/views/Login/konto.blade.php
@@ -0,0 +1,30 @@
+@extends('Template.layout')
+
+
+@section('main')
+
+    @extends('Template.links')
+
+<link rel="stylesheet" href="{{URL::asset("CSS/dashboard.css")}}">
+@if(isset($info))
+    {{$info}}
+@endif
+<div id="p2" class="loading">
+    <div class="close" onclick="myFunction(p2, 'p2')">X</div>
+    <h1>Passwort ändern</h1>
+    <form action="/konto/passwortAendern" method="post">
+        @csrf
+        <input type="password" placeholder="Altes Passwort" name="opassword">
+        <input type="password" placeholder="Neues Passwort" name="npassword"><br><br>
+        <button name="passwortChange" type="submit" class="big-buttons">Submit</button>
+    </form>
+</div>
+@if(isset($fehler_menu))
+    {{$fehler_menu}}
+    <script> myFunction(p2, 'p2'); </script>
+@endif
+
+<div>
+<button class="big-buttons" onclick="myFunction(p2, 'p2')">Passwort ändern</button>
+</div>
+@endsection
---
+++ resources/views/Template/links.blade.php
@@ -0,0 +1,14 @@
+@section('links')
+@if ((isset($_SESSION['HiWi_UserId'])))
+    <li class="header-li"><a href="/Student/dashboard">Dashboard Student</a></li>
+    <li class="header-li"><a href="/Tutor/dashboard">Dashboard HiWi</a></li>
+@elseif((isset($_SESSION['Student_UserId'])))
+    <li class="header-li"><a href="/Student/dashboard">Dashboard</a></li>
+@elseif((isset($_SESSION['WiMi_UserId'])))
+    <li class="header-li"><a href="/Tutor/dashboard">Dashboard WiMi</a></li>
+@elseif((isset($_SESSION['Prof_UserId'])))
+    <li class="header-li"><a href="/Professor/dashboard">Dashboard</a></li>
+@elseif((isset($_SESSION['PA_UserId'])))
+    <li class="header-li"><a href="/pruefungsamt">Dashboard</a></li>
+@endif
+@endsection
