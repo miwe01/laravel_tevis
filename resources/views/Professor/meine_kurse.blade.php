0c0c5e848378c0182b903db773c00604f51c66a2
ghalia <ghaliabenslimen506@gmail.com>
2021-01-11 18:25:03 +0100

    meine_kurse Seite ändeurngen

--- resources/views/Professor/meine_kurse.blade.php
+++ resources/views/Professor/meine_kurse.blade.php
@@ -8,44 +8,20 @@
 <h1 class ="meinekurse">Meine Kurse</h1>
 <button class="b2"> neuen kurse anlegen </button>
 
-        <div class="grid1">WiSe19/20</div>
+@foreach($kurse as $kurs)
 
-        <div>
+    <div class="grid2">{{ $kurs->Semester }} {{ $kurs->Jahr }}</div>
+
+    <div>
         <ul>
-            <li class ="kurse"><a href="Kurs1.html">Kurse1</a> </li>
+            <li class ="kurse"><a href="Kurs1.html">{{$kurs->Modulname}}</a> </li>
             <br>
             <li class="li1" >an der veranstaltung </li>
-            <li>menge der gruppen</li>
+            <li>
+                {{$kurs->mengeDerGruppen}} menge der gruppen</li>
             <li>anzahl der tn</li>
+
         </ul>
-            <br>
-<ul>
-    <li class ="kurse"><a href="Kurs1.html">Kurse2</a> </li>
-    <br>
-    <li class ="li1">an der Veranstaltung</li>
-    <li>Menge der  gruppen</li>
-    <li>anzahl der tn</li>
-</ul>
-        </div>
-
-<div class="grid2">SoSe20/21</div>
-
-<div>
-    <ul>
-        <li class ="kurse"><a href="Kurs1.html">Kurse1</a> </li>
-        <br>
-        <li class="li1" >an der veranstaltung </li>
-        <li> menge der gruppen</li>
-        <li>anzahl der tn</li>
-    </ul>
-    <br>
-    <ul>
-        <li class ="kurse"><a href="Kurs2.html">Kurse2</a> </li>        <br>
-        <li class ="li1">an der Verantaltung</li>
-        <li>Menge der  gruppen</li>
-        <li>anzahl der tn</li>
-    </ul>
-</div>
-
-
+    </div>
+@endforeach
 @endsection
