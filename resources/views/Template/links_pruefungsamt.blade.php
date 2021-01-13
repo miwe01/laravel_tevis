511f116121f380f827c0d3025ee5fcc32453e334
Mika <wemi99@outlook.de>
2021-01-13 20:42:34 +0100

    Links removed

--- resources/views/Template/links_pruefungsamt.blade.php
+++
@@ -1,4 +0,0 @@
-@section('links')
-    <li class="header-li"><a href="/pruefungsamt">Dashboard</a></li>
-    <li class="header-li"><a href="/pruefungsamt/konto">Mein Konto</a></li>
-@endsection
--- resources/views/Template/links_student.blade.php
+++
@@ -1,4 +0,0 @@
-@section('links')
-    <li class="header-li"><a href="/Student/main_Student">Dashboard</a></li>
-    <li class="header-li"><a href="/Student/konto_Student">Mein Konto</a></li>
-@endsection
