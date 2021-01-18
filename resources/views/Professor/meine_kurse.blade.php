365c31941b28a8ed24899e1da85e0acd6e742171
Tom <tom.becker@alumni.fh-aachen.de>
2021-01-18 14:15:38 +0100

    meinekurs update

--- resources/views/Professor/meine_kurse.blade.php
+++ resources/views/Professor/meine_kurse.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="{{URL::asset("CSS/styleProfessor_mKurse.css")}}">
 
     <h1 class ="meinekurse">Meine Kurse</h1>
-    <form  action="/Professor/kurs/new" method="get" >
+    <form  action="/Professor/kurs/new/group" method="get" >
         @csrf
         <input type="hidden" value="{{$gruppen[0]->Modulnummer}}" name="GruppenID" id="kursanlegen">
         <button class="b2" type="submit" id="kursanlegen"> neuen kurse anlegen </button>
