37e6678eb03cb68ddc4e8a30aa5aab371d5b93e9
SPfundel <74179736+SPfundel@users.noreply.github.com>
2021-01-19 20:28:58 +0100

    Update new_course.blade.php

--- resources/views/Professor/new_course.blade.php
+++ resources/views/Professor/new_course.blade.php
@@ -14,13 +14,26 @@
     <form action="/Professor/meine_kurse/createCourse" method="post">
         @csrf
 
-        <div align="center "class="area">
-            <p>{{__("Name")}}: <input  class ="style1" type="text" id="ModulName" name="ModulName" ></p>
-            <p>{{__("Raum")}}: <input class ="style1 " name="Raum" id="Raum" type="text"></p>
-            <p>{{__("Modulnummer")}}: <input type="number" id="Modulnummer" name="Modulnummer" min="10000" max="99999"></p>
-            <p>{{__("Jahr")}}: <input type="number" id="Jahr" min="{{$aktuellesJahr - 1}}" max="{{$aktuellesJahr + 1}}" name="Jahr"></p>
-            <p>{{__("Semester")}}: <input type="text" id="Semester" name="Semester"></p>
-            <div/>
-            <br>
-            <button style="font-weight: bolder ;background-color: cadetblue" type="submit">Kurs Hinzufügen </button>
+        <label for=ModulName>Name: </label>
+        <input type="text" id="ModulName" name="ModulName" />
+        <br>
+        <br>
+        <label for="Raum">Raum: </label>
+        <input type="text" id="Raum" name="Raum" />
+        <br>
+        <br>
+        <label for="Modulnummer">Modulnummer: </label>
+        <input type="number" id="Modulnummer" name="Modulnummer" min="10000" max="99999" />
+        <br>
+        <br>
+        <label for="Jahr">Jahr: </label>
+        <input type="number" id="Jahr" min="{{$aktuellesJahr - 1}}" max="{{$aktuellesJahr + 1}}" name="Jahr" />
+        <br>
+        <br>
+        <div><label for="Semester">Semester: </label>
+        <input type="text" id="Semester" name="Semester" />
+        <br>
+        <br>
+        <button style="font-weight: bolder ;background-color: cadetblue" type="submit">Kurs Hinzufügen </button>
+
 @endsection
