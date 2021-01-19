4739536a41cc6f9c82d35b8915763846080c195c
SPfundel <74179736+SPfundel@users.noreply.github.com>
2021-01-19 17:48:58 +0100

    Update new_course.blade.php

--- resources/views/Professor/new_course.blade.php
+++ resources/views/Professor/new_course.blade.php
@@ -3,9 +3,10 @@
 
 @section('main')
 
-    <link rel="stylesheet" href="{{URL::asset("CSS/styleProfessor_gruppe.css")}}">
-
-    <h1>Kurs Anlegen</h1>
+    <link rel="stylesheet" href="{{URL::asset("CSS/styleProfessor.css")}}">
+    <br>
+    <h1 align="center">{{__("Kurs Anlegen")}}</h1>
+    <br>
 
     @if (isset($msg))
         {{$msg}}
@@ -13,21 +14,13 @@
     <form action="/Professor/meine_kurse/createCourse" method="post">
         @csrf
 
-        <label style="font-weight: bolder; font-family: Verdana,serif;color: darkslategrey;" for="ModulName">Module Name:</label>
-        <input  class ="style1" type="text" id="ModulName" name="ModulName" >
-        <br>
-        <label style="font-weight: bolder; font-family: Verdana,serif;color: darkslategrey;" for="Raum">Raum</label>
-        <input class ="style1 " name="Raum" id="Raum" type="text">
-        <br>
-        <label style="font-weight: bolder" for="Modulnummer">Modulnummer </label>
-        <input type="number" id="Modulnummer" name="Modulnummer" min="10000" max="99999">
-        <br>
-        <label style="font-weight: bolder" for="Jahr">Jahr</label>
-        <input type="number" id="Jahr" min="{{$aktuellesJahr - 1}}" max="{{$aktuellesJahr + 1}}" name="Jahr">
-        <br>
-        <label style="font-weight: bolder" for="Semester">Semester</label>
-        <input type="text" id="Semester" name="Semester">
+        <div align="center "class="area">
+        <p>{{__("Name")}}: <input  class ="style1" type="text" id="ModulName" name="ModulName" ></p>
+        <p>{{__("Raum")}}: <input class ="style1 " name="Raum" id="Raum" type="text"></p>
+        <p>{{__("Modulnummer")}}: <input type="number" id="Modulnummer" name="Modulnummer" min="10000" max="99999"></p>
+        <p>{{__("Jahr")}}: <input type="number" id="Jahr" min="{{$aktuellesJahr - 1}}" max="{{$aktuellesJahr + 1}}" name="Jahr"></p>
+        <p>{{__("Semester")}}: <input type="text" id="Semester" name="Semester"></p>
+        <div/>
         <br>
         <button style="font-weight: bolder ;background-color: cadetblue" type="submit">Kurs Hinzufügen </button>
-    </form>
 @endsection
