3ee9a843b30f9a1a8707a99bd01fcfa6123c8bc3
SPfundel <74179736+SPfundel@users.noreply.github.com>
2021-01-20 01:05:22 +0100

    Update new_course.blade.php

--- resources/views/Professor/new_course.blade.php
+++ resources/views/Professor/new_course.blade.php
@@ -8,32 +8,30 @@
     <h1 align="center">{{__("Kurs Anlegen")}}</h1>
     <br>
 
-    @if (isset($msg))
-        {{$msg}}
-    @endif
     <form action="/Professor/meine_kurse/createCourse" method="post">
         @csrf
 
         <label for=ModulName>Name: </label>
         <input type="text" id="ModulName" name="ModulName" />
-        <br>
-        <br>
+        <br><br>
         <label for="Raum">Raum: </label>
         <input type="text" id="Raum" name="Raum" />
-        <br>
-        <br>
+        <br><br>
         <label for="Modulnummer">Modulnummer: </label>
         <input type="number" id="Modulnummer" name="Modulnummer" min="10000" max="99999" />
-        <br>
-        <br>
+        <br><br>
         <label for="Jahr">Jahr: </label>
         <input type="number" id="Jahr" min="{{$aktuellesJahr - 1}}" max="{{$aktuellesJahr + 1}}" name="Jahr" />
-        <br>
-        <br>
-        <div><label for="Semester">Semester: </label>
+        <br><br>
+        <label for="Semester">Semester: </label>
         <input type="text" id="Semester" name="Semester" />
-        <br>
-        <br>
-        <button style="font-weight: bolder ;background-color: cadetblue" type="submit">Kurs Hinzufügen </button>
+        <br><br><br>
+        <button type="submit">Kurs Hinzufügen </button>
+    </form>
+    <br>
+
+    @if (isset($msg))
+        {{$msg}}
+    @endif
 
 @endsection
